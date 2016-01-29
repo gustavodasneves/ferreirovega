689379d3703e7655ad7900241fdeb52876d71870
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2016-01-29 16:02:59 -0200

    atualizado arquivos de tradução

--- catalog/language/portuguese-br/total/coupon.php
+++ catalog/language/portuguese-br/total/coupon.php
@@ -1,3 +1,15 @@
 <?php
+
+// Heading
+$_['heading_title'] = 'Usar Cupom de Desconto';
+
 // Text
 $_['text_coupon'] = 'Cupom (%s)';
+$_['text_success']  = 'Sucesso: Seu cupom de desconto foi aplicado!';
+
+// Entry
+$_['entry_coupon']  = 'Digite aqui o código do seu cupom';
+
+// Error
+$_['error_coupon']  = 'Atenção: Seu cupom é inválido, expirou ou chegou ao limite de usos!';
+$_['error_empty']   = 'Atenção: Por favor, digite o código do seu cupom!';
--- catalog/language/portuguese-br/total/voucher.php
+++ catalog/language/portuguese-br/total/voucher.php
@@ -1,3 +1,15 @@
 <?php
+
+// Heading
+$_['heading_title'] = 'Usar Vale-Presente';
+
 // Text
-$_['text_voucher'] = 'Vale-Presente (%s)';
+$_['text_voucher']  = 'Vale-Presente (%s)';
+$_['text_success']  = 'Sucesso: O desconto do seu vale-presente foi aplicado!';
+
+// Entry
+$_['entry_voucher'] = 'Digite aqui o código do seu vale-presente';
+
+// Error
+$_['error_voucher'] = 'Atenção: O código do seu vale-presente é inválido, expirou ou já foi usado!';
+$_['error_empty']   = 'Atenção: Por favor, digite um código de vale-presente!';
